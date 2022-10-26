159469f17f2f013b7dcd1684fe0bd691fd4b936f
Erik <esnetto@gmail.com>
2022-10-26 19:44:19 +0000

    paste function

--- contracts/NOF-SC.sol
+++ contracts/NOF-SC.sol
@@ -1,19 +1,14 @@
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Counters.sol";
 
 import "./ContextMixin.sol";
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.4;
 
-contract NOF_Alpha is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, ContextMixin {
-    using Counters for Counters.Counter;
-
-    Counters.Counter private _tokenIdCounter;
+contract NOF_Alpha is ERC721, ERC721URIStorage, Ownable, ContextMixin {
 
     // NOF Alpha Custom Code --> 
     string public baseUri = "https://gateway.pinata.cloud/ipfs/QmZuSMk8d8Xru6J1PKMz5Gt6Qq8qVQ1Ak8p661zdGmGbGx/";
@@ -25,11 +20,13 @@ contract NOF_Alpha is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, Conte
     }
 
     struct Card {
-        string  class;
-        string  collection;
+        uint  class;
+        uint  collection;
         string  season;
         uint    completion;
     }
+
+    uint public totalCardsCounter;
 
     mapping (string => Season) public seasons;
     mapping (uint => Card) public cards;
@@ -39,22 +36,21 @@ contract NOF_Alpha is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, Conte
     uint nextCard;
     // <-- NOF Alpha Custom Code
 
-    constructor() ERC721("NOF Alpha", "NOFA") {
-        _tokenIdCounter.increment();
-    }
+    constructor() ERC721("NOF Alpha", "NOFA") {}
 
-    function mint(address to, string memory uri) internal {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _mint(to, tokenId);
-        _setTokenURI(tokenId, uri);
+    function mint(address to, string memory uri, uint _class, uint _collection, string memory _season, uint carNumber) internal {
+        cards[carNumber].class = _class;
+        cards[carNumber].collection = _collection;
+        cards[carNumber].season = _season;
+        _mint(to, carNumber);
+        _setTokenURI(carNumber, uri);
     }
 
     // The following functions are overrides required by Solidity.
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
-        override(ERC721, ERC721Enumerable)
+        override(ERC721)
     {
         super._beforeTokenTransfer(from, to, tokenId);
     }
@@ -75,7 +71,7 @@ contract NOF_Alpha is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, Conte
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721Enumerable)
+        override(ERC721)
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
@@ -84,7 +80,7 @@ contract NOF_Alpha is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, Conte
         function isApprovedForAll(
         address _owner,
         address _operator
-    ) public override(ERC721, IERC721) view returns (bool isOperator) {
+    ) public override(ERC721) view returns (bool isOperator) {
       // if OpenSea's ERC721 Proxy Address is detected, auto-return true
       // for Polygon's Mumbai testnet, use 0xff7Ca10aF37178BdD056628eF42fD7F799fAc77c
         if (_operator == address(0x58807baD0B376efc12F5AD86aAc70E78ed67deaE)) {
@@ -119,7 +115,7 @@ contract NOF_Alpha is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, Conte
             uint cardNum = seasons[name].albumns[index];
             seasons[name].albumns[index] = seasons[name].albumns[seasons[name].albumns.length - 1];
             seasons[name].albumns.pop();
-            mint(msg.sender, string.concat(baseUri, toString(cardNum)));
+            mint(msg.sender, string.concat(baseUri, toString(cardNum)), 0, cardNum/6-1, name, cardNum);
         }
         //transfer figus
         for(uint i ; i < 5; i++) {
@@ -127,21 +123,24 @@ contract NOF_Alpha is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, Conte
             uint cardNum = seasons[name].cards[index];
             seasons[name].cards[index] = seasons[name].cards[seasons[name].cards.length - 1];
             seasons[name].cards.pop();
-            mint(msg.sender,  string.concat(baseUri, toString(cardNum)));
+            mint(msg.sender,  string.concat(baseUri, toString(cardNum)), 1, cardNum/6, name, cardNum);
         }
     }
 
     //Genera una nueva temporada con el nombre, precio de cartas y cantidad de cartas (debe ser multiplo de 6)
     function newSeason(string memory name, uint price, uint amount) public onlyOwner {
         seasons[name].price = price;
+        
         for(uint i = 1; i <= amount; i++) {
             if(i % 6 == 0) {
-                seasons[name].albumns.push(i);
+                seasons[name].albumns.push(i+totalCardsCounter);
             } else {
-                seasons[name].cards.push(i);
+                seasons[name].cards.push(i+totalCardsCounter);
             }
             
         }
+
+        totalCardsCounter = totalCardsCounter + amount;
     }
 
     //Devuelve un array con las cartas disponibles
@@ -152,6 +151,21 @@ contract NOF_Alpha is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, Conte
     //Devuelve un arrary con los albumns disponibles
     function getSeasonAlbums(string memory name) public view returns(uint[] memory) {
         return seasons[name].albumns;
+    }
+
+    function pasteCards(uint card, uint albumn) public {
+        require(ownerOf(card) == msg.sender, "This is not your card");
+        require(ownerOf(albumn) == msg.sender, "This is not your albumn");
+        require(cards[card].collection == cards[albumn].collection, "cards is not from the same collection");
+
+        _burn(card);
+        cards[albumn].completion++;
+
+        if( cards[albumn].completion == 5){
+            _setTokenURI(albumn, string.concat(baseUri, toString(albumn), "F"));
+        }
+        
+
     }
 
     function toString(uint256 value) internal pure returns (string memory) {
@@ -174,6 +188,10 @@ contract NOF_Alpha is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, Conte
             value /= 10;
         }
         return string(buffer);
+    }
+
+    function withdrawDAI(uint amount) public onlyOwner {
+        IERC20(DAI_TOKEN).transferFrom(address(this), msg.sender, amount);
     }
     // <-- NOF Alpha Custom Code
 }
