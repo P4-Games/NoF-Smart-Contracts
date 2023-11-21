3a9f6367efeddf2266ba948639cb07cc15915b5e
dappsar <dapps.ar@gmail.com>
2023-11-21 13:23:43 -0300

    corrected text in tests

--- test/gamma.packs.test.ts
+++ test/gamma.packs.test.ts
@@ -5,28 +5,28 @@ import { deployNofFixture } from './common'
 
 describe('NoF - Gamma Packs Tests', function () {
 
-  it('addOwner should revert when address is invalid', async () => {
+  it('Add owner should revert when the address is invalid', async () => {
     const { gammaCards } = await loadFixture(deployNofFixture)
     await expect(gammaCards.addOwner(ethers.constants.AddressZero)).to.be.revertedWith("Invalid address.")
   });
 
-  it('addOwner should revert when adding an existing owner', async () => {
+  it('Add owner should revert when adding an existing owner', async () => {
     const { gammaCards, address1 } = await loadFixture(deployNofFixture)
     await gammaCards.addOwner(address1.address)
     await expect(gammaCards.addOwner(address1.address)).to.be.revertedWith("Address is already an owner.")
   });
 
-  it('removeOwner should revert when address is invalid', async () => {
+  it('Remove owner should revert when the address is invalid', async () => {
     const { gammaCards } = await loadFixture(deployNofFixture)
     await expect(gammaCards.removeOwner(ethers.constants.AddressZero)).to.be.revertedWith("Invalid address.")
   });
 
-  it('removeOwner should revert when removing self as an owner', async () => {
+  it('Remove owner should revert when removing self as an owner', async () => {
     const { gammaCards, address0 } = await loadFixture(deployNofFixture)
     await expect(gammaCards.removeOwner(address0.address)).to.be.revertedWith("You cannot remove yourself as an owner.")
   });
 
-  it('removeOwner should revert when removing a non-existing owner', async () => {
+  it('Remove owner should revert when removing a non-existing owner', async () => {
     const { gammaCards } = await loadFixture(deployNofFixture)
     const nonExistingOwner = ethers.Wallet.createRandom().address
     await expect(gammaCards.removeOwner(nonExistingOwner)).to.be.revertedWith("Address is not an owner.")
