64f76054c04ac4bdb0fdfc92d0848ac5a9114d3b
dappsar <dapps.ar@gmail.com>
2023-12-05 19:27:33 -0300

    [feat] :sparkles: add function to validate if allowed to finish album

--- test/common.ts
+++ test/common.ts
@@ -103,9 +103,49 @@ function printOffers(offers: any[], called: string) {
   }
 }
 
+async function allowedToFinishAlbum (gammaCards: any, daiContract: any, address: any) {
+  // Hay 4 condicione sen el contrato para poder completarlo:
+  // 1. Que el usuario tengan un álbum: require(cardsByUser[msg.sender][120] > 0, "No tienes ningun album");
+  // 2. Que haya un balance mayor a lo que se paga de premio: require(prizesBalance >= mainAlbumPrize, "Fondos insuficientes");
+  // 3. Que el usuario tenga todas las cartas.
+  // 4. Que el contrato tenga un balance superior al precio del premio (mainAlbumPrize)
+  // Las 4 se validan en el contrato y aquí (para evitar la llamada al contrato)
+
+  // require(cardsByUser[msg.sender][120] > 0, "No tienes ningun album");
+  const userHasAlbum = await gammaCards.cardsByUser(address, 120)
+  const prizesBalance = await gammaCards.prizesBalance()
+  const mainAlbumPrize = await gammaCards.mainAlbumPrize()
+  const gammaContractBalance = await daiContract.balanceOf(gammaCards.address)
+  const prizeBalanceFormatted = ethers.utils.formatUnits(prizesBalance, 18)
+  const albumPrizeFormatted = ethers.utils.formatUnits(mainAlbumPrize, 18)
+  const gammaContractBalanceFormatted = ethers.utils.formatUnits(gammaContractBalance, 18)
+
+  // require(prizesBalance >= mainAlbumPrize, "Fondos insuficientes");
+  const prizesBalanzGTAlbumPrice = parseInt(prizeBalanceFormatted) >= parseInt(albumPrizeFormatted)
+
+  // require gammaCardContractBalance >= mainAlbumPrize
+  const contractBalanzGTAlbumPrice =
+    parseInt(gammaContractBalanceFormatted) >= parseInt(albumPrizeFormatted)
+
+  const result = userHasAlbum && prizesBalanzGTAlbumPrice && contractBalanzGTAlbumPrice
+
+  log('prizesBalanzGTAlbumPrice', {
+    userHasAlbum,
+    prizeBalanceFormatted,
+    albumPrizeFormatted,
+    gammaContractBalanceFormatted,
+    prizesBalanzGTAlbumPrice,
+    contractBalanzGTAlbumPrice,
+    result
+  })
+  
+  return result
+}
+
 export {
   nofDaiContractName, nofAlphaContractName, 
   nofGammaPacksContractName, nofGammaCardsContractName,
   nofGammaOffersContractName, deployNofFixture,
-  getOnePackData, getCardsByUserType, log, printOffers
+  getOnePackData, getCardsByUserType, log, printOffers,
+  allowedToFinishAlbum
 }
