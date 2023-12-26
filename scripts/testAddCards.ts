94b49357bef242d31acb2216b83ec851adc135f2
dappsar <dapps.ar@gmail.com>
2023-12-26 15:23:35 -0300

    [build] :arrow_up: upgrade open-zeppelin dependency (#108)

--- scripts/testAddCards.ts
+++ scripts/testAddCards.ts
@@ -13,7 +13,7 @@ async function main() {
     console.log(`Working with the account ${acc}, current acc balance: ${ethers.utils.formatEther(balance)}\n`)
 
     const wallet = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'
-    const cardsContract = await ethers.getContractAt('NofGammaCardsV5', '0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9')
+    const cardsContract = await ethers.getContractAt('NofGammaCardsV5', '0x5FC8d32690cc91D4c39d9d3abcBD16989F875707')
     await cardsContract.testAddCards(wallet)
 
     process.exit(0)
