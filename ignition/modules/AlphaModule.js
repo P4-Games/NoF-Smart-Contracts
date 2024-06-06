e03226c2f4c146bdbe6924e40b273fb09be83094
Tomas <tomasfreire@gmail.com>
2024-06-06 12:31:06 -0300

    [build] removed ignition module

--- ignition/modules/AlphaModule.js
+++
@@ -1,8 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("AlphaModule", (m) => {
-
-  const alpha = m.contract("AlphaV4");
-
-  return { alpha };
-});
